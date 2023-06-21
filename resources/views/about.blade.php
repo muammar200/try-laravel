65c9c285d123c4caed1638977fcf6b1f029bc532
muammar200 <amuammar01@gmail.com>
2023-06-21 15:46:26 +0800

    Instalasi Laravel, memahami struktur folder, Routes, dan View

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ammar Blog | Home</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <p><?=$name;?></p>
+    <p><?=$email;?></p>
+    <p><?=$age;?></p>
+</body>
+</html>
